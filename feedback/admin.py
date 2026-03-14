d52b85d42b167c286f7efa4da9e84add22719e14
Mong kieu <kieu_2251220038@dau.edu.vn>
2026-03-14 18:27:48 +0700

    first commit

---
+++ feedback/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Feedback
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'room', 'rating', 'country','created_at')
+    search_fields = ('name', 'email', 'room__name')
+    list_filter = ('rating', 'created_at')
+    ordering = ('-created_at',)
---
+++ hotel_proj/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HotelProjConfig(AppConfig):
+    name = 'hotel_proj'
